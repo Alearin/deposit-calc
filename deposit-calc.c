49004a72bb65881a93e6927b439adda1195fce44
Михайлова Елена Денисовна <ri68s11@mwp09.csc.local>
2017-02-20 14:26:32 +0700

    Created a function to calculate the amount

--- deposit-calc.c
+++ deposit-calc.c
@@ -7,6 +7,26 @@ void Input_Validation(int* term, int* amount){
         printf("incorrect data of amount\n");
 	
 }
+
+int Calculation_of_income(int* term, int* amount){
+	int income = 0;
+	if(*amount < 100000){
+		if(*term <= 30){
+			income = *amount * 0.9;
+		}
+		else if((*term >= 31)  && (*term <= 120)){
+			income = *amount * 1.02;
+		}
+		else if((*term >= 121)  && (*term <= 240)){
+			income = *amount * 1.06;
+		}
+		else if((*term >= 241)  && (*term <= 365)){
+			income = *amount * 1.12;
+		}
+	}
+	
+	return income;
+}
 		
 void main(){
 
@@ -19,4 +39,5 @@ void main(){
 	scanf("%d", &amount);
 	
 	Input_Validation(&term, &amount);
+	printf("%d\n", Calculation_of_income(&term, &amount));
 }
